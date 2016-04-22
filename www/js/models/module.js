b02c3577860b2704b6fff09517ac2a9775ba24fa
Craig Franklin <craigjfranklin@gmail.com>
2016-04-23 09:55:23 +1000

    add script tags, folders, model

---
+++ www/js/models/module.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var bbApp = bbApp || {};
+
+( function () {
+
+  bbApp.Module = Backbone.Model.extend({
+    defaults: {
+      activities: [{
+        name: 'activity 1'
+        reception: {
+          audio: 'audio-file',
+          text: 'text'
+        },
+        textInput: {
+          audio: 'audio-file',
+          options: [
+            {
+              name: 'option 1',
+              correct: true
+            },
+            {
+              name: 'option 2',
+              correct: true
+            },
+            {
+            name: 'option 3',
+            correct: true
+            },
+            {
+            name: 'option 4',
+            correct: true
+            }
+          ]
+        },
+        audioInput: {
+          audio: 'audio-file',
+          text: 'text'
+        }
+      }]
+    }
+  });
+})();
